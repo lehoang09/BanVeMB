490dc63c5b151fbd5e6195ab4d24f119c98742db
lehoang09 <34390408+lehoang09@users.noreply.github.com>
2017-12-28 21:17:32 +0700

    Frm hóa đơn vs chi tiết
    Code cho frm Chi tiết và hóa đơn ngày hôm nay ( Chưa hoàn thiện phần chi tiết mới tạo khung vs đổ dữ liệu )

---
+++ ChiTietCtrl.cs
@@ -0,0 +1,27 @@
+﻿using QLBanVeMayBay.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBanVeMayBay.Control
+{
+    class ChiTietCtrl
+    {
+        ChiTietMod ctMod = new ChiTietMod();
+        public DataTable GetData(string ma)
+        {
+            return ctMod.GetData(ma);
+        }
+        public bool AddData(DataTable dt)
+        {
+            return ctMod.AddData(dt);
+        }
+        public bool DelData(string ma)
+        {
+            return ctMod.DelData(ma);
+        }
+    }
+}
---
+++ HoaDonCtrl.cs
@@ -0,0 +1,28 @@
+﻿using QLBanVeMayBay.Model;
+using QLBanVeMayBay.Object;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBanVeMayBay.Control
+{
+    class HoaDonCtrl
+    {
+        HoaDonMod hdMod = new HoaDonMod();
+        public DataTable GetData()
+        {
+            return hdMod.GetData();
+        }
+        public bool AddData(HoaDonObj hdObj)
+        {
+            return hdMod.AddData(hdObj);
+        }
+        public bool DelData(string ma)
+        {
+            return hdMod.DelData(ma);
+        }
+    }
+}
