67c11caad9f385ae697a2de723e937d9e3eb7e44
anhminh99 <34390751+anhminh99@users.noreply.github.com>
2017-12-30 06:54:36 -0800

    Update code May Bay
    Sửa code MB

---
+++ MayBayMod.cs
@@ -0,0 +1,140 @@
+﻿using QLBanVeMayBay.Object;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBanVeMayBay.Model
+{
+    class MayBayMod
+    {
+
+        ConnectToSQL con = new ConnectToSQL();
+        SqlCommand cmd = new SqlCommand();
+
+        public DataTable GetData()
+        {
+            DataTable dt = new DataTable();
+            cmd.CommandText = "select * from tb_MayBay";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+            try
+            {
+                con.OpenConn();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                string mex = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return dt;
+        }
+
+        public DataTable GetData(string dieukien)
+        {
+            DataTable dt = new DataTable();
+            cmd.CommandText = "select * from tb_MayBay " + dieukien;
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+            try
+            {
+                con.OpenConn();
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                sda.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                string mex = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return dt;
+        }
+
+        public bool AddData(MayBayObj hhObj)
+        {
+            cmd.CommandText = "Insert into tb_MayBay values ('" + hhObj.MaMayBay+ "', N'" + hhObj.TenMayBay + "','" + hhObj.DonGia + "',"+hhObj.SoLuong+" )";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+            try
+            {
+                con.OpenConn();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mex = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return false;
+        }
+
+        public bool UpdData(MayBayObj hhObj)
+        {
+            cmd.CommandText = "Update tb_MayBay set TenMB =  N'" + hhObj.TenMayBay + "', SoLuong = " + hhObj.SoLuong + ", DonGia = " + hhObj.DonGia + " Where MaMB= '" + hhObj.MaMayBay + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+            try
+            {
+                con.OpenConn();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mex = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return false;
+        }
+
+        public bool UpdSL(string mahh, int SL)
+        {
+            cmd.CommandText = "Update tb_MayBay set  SoLuong = " + SL + " Where MaMB= '" + mahh + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+            try
+            {
+                con.OpenConn();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mex = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return false;
+        }
+
+        public bool DelData(string ma)
+        {
+            cmd.CommandText = "Delete tb_MayBay Where MaMB= '" + ma + "'";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = con.Connection;
+            try
+            {
+                con.OpenConn();
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                string mex = ex.Message;
+                cmd.Dispose();
+                con.CloseConn();
+            }
+            return false;
+        }
+    }
+}
---
+++ MayBayObj.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLBanVeMayBay.Object
+{
+    class MayBayObj
+    {
+        string ma, ten, diemdi, diemden;
+
+        public string DiemDi
+        {
+            get { return diemdi; }
+            set { diemdi = value; }
+        }
+
+        public string DiemDen
+        {
+            get { return diemden; }
+            set { diemden = value; }
+        }
+        int dongia, soluong;
+
+        public int SoLuong
+        {
+            get { return soluong; }
+            set { soluong = value; }
+        }
+
+        public int DonGia
+        {
+            get { return dongia; }
+            set { dongia = value; }
+        }
+
+        public string TenMayBay
+        {
+            get { return ten; }
+            set { ten = value; }
+        }
+
+        public string MaMayBay
+        {
+            get { return ma; }
+            set { ma = value; }
+        }
+     
+
+        public MayBayObj() { }
+
+
+        public MayBayObj(string ma, string ten, int dongia, int soluong,string diemdi,string diemden)
+        {
+            this.ma = ma;
+            this.ten = ten;
+            this.dongia = dongia;
+            this.soluong = soluong;
+            this.diemdi = diemdi;
+            this.diemden = diemden;
+
+        }
+    
+    }
+}
